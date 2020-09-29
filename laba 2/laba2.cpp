ab6fda0935c2e0b99c533ff2330824b06b815c99
Daniil Orlov <D4nnisimo@yandex.ru>
2020-09-29 11:19:29 +0700

    Функция input

--- laba 2/laba2.cpp
+++ laba 2/laba2.cpp
@@ -24,7 +24,23 @@ laptop_factory init(int ram, int freq, double pr, int year)
     return check;
 }
 
-
+laptop_factory input()
+{
+    laptop_factory lap;
+    init(0, 0, 0, 0);
+    printf("Enter model: \n");
+    rewind(stdin); 
+    gets_s(lap.model, LEN);
+    printf("Enter RAM size : \n");
+    scanf_s("%d", &lap.RAM_size);
+    printf("Enter CPU frequency : \n");
+    scanf_s("%d", &lap.CPU_frequency);
+    printf("Enter RAM size : \n");
+    scanf_s("%f", &lap.price);
+    printf("Enter year : \n");
+    scanf_s("%d", &lap.year);
+    return lap;
+}
 
 
 int main()
